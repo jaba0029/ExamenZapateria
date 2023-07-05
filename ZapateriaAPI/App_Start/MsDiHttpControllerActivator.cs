7becac83e7811f05805422d637f164135ea86453
tibemcamacho <33906717+tibemcamacho@users.noreply.github.com>
2023-07-05 09:20:40 -0600

    Se actualiza la version

---
+++ ZapateriaAPI/App_Start/MsDiHttpControllerActivator.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http.Controllers;
+using System.Web.Http.Dispatcher;
+
+//namespace ZapateriaAPI.App_Start
+//{
+public class MsDiHttpControllerActivator : IHttpControllerActivator
+{
+    private readonly ServiceProvider provider;
+
+    public MsDiHttpControllerActivator(ServiceProvider provider)
+    {
+        this.provider = provider;
+    }
+
+    public IHttpController Create(
+        HttpRequestMessage request, HttpControllerDescriptor descriptor, Type type)
+    {
+        var scope = this.provider.CreateScope();
+        request.RegisterForDispose(scope);
+        return (IHttpController)scope.ServiceProvider.GetRequiredService(type);
+    }
+}
+//}
--- ZapateriaAPI/Global.asax.cs
+++ ZapateriaAPI/Global.asax.cs
@@ -1,11 +1,18 @@
+using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Http;
+using System.Web.Http.Dispatcher;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using Zapateria_Models;
+using ZapateriaAPI.BussinessLayer.Services.Implementations;
+using ZapateriaAPI.BussinessLayer.Services.Interfaces;
+using ZapateriaAPI.DataAccessLayer.Repository.Implementations;
+using ZapateriaAPI.DataAccessLayer.Repository.Interfaces;
 
 namespace ZapateriaAPI
 {
@@ -18,6 +25,27 @@ namespace ZapateriaAPI
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+            var services = new ServiceCollection();
+
+            // Register all your controllers and other services here:
+            services.AddTransient<Controllers.StoresController>();
+            services.AddTransient<Controllers.ArticlesController>();
+            services.AddTransient<ShopContext>();
+            services.AddScoped<IStoreService, StoreService>();
+            services.AddScoped<IStoreRepository, StoreRepository>();
+            services.AddScoped<IArticlesService, ArticlesService>();
+            services.AddScoped<IArticlesRepository, ArticlesRepository>();
+
+            var provider = services.BuildServiceProvider(new ServiceProviderOptions
+            {
+                // Prefer to keep validation on at all times
+                ValidateOnBuild = true,
+                ValidateScopes = true
+            });
+
+            GlobalConfiguration.Configuration.Services.Replace(
+                typeof(IHttpControllerActivator),
+                new MsDiHttpControllerActivator(provider));
         }
     }
 }
